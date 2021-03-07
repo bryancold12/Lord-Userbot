cf6e1121aaf0a0b7a53cc0af0a9978c15c737120
bryancold12 <80190554+bryancold12@users.noreply.github.com>
2021-03-07 07:57:42 +0000

    Update kekuatan.py

--- userbot/modules/kekuatan.py
+++ userbot/modules/kekuatan.py
@@ -69,7 +69,7 @@ async def _(fry):
 
 @register(outgoing=True, pattern=r'^.df(:? |$)([1-8])?')
 async def _(fry):
-    await fry.edit("`Sedang Dalam Proses......`")
+    await fry.edit("`Sedang Dalam Merusak......`")
     level = fry.pattern_match.group(2)
     if fry.fwd_from:
         return
